58581dc0e34f8c02defe930fc787a65743f6c3cc
Raffi Sanna <rvs314@protonmail.com>
2019-05-25 18:31:17 -0400

    basic JS

--- index.js
+++ index.js
@@ -2,5 +2,33 @@
 
 function startquery() {
     var query = document.getElementById("query").value
-    console.log(query)
+    wikipedia(query).then((j) => {
+        console.log(j)
+    })
+}
+
+function article(nm, src, blrb, rnk, url) {
+    let art = {}
+    art.name = nm
+    art.source = src
+    art.blurb = blrb
+    art.rank = rnk
+    art.url = url
+    return art
+}
+
+function strip(html){
+    var doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+}
+
+function wikipedia(query) {
+    const apiurl="https://en.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&origin=*&srsearch="
+    const fullurl = apiurl + escape(query)
+    return fetch(fullurl)
+        .then(response => response.json())
+        .then((j) => {
+            const pages = j.query.search
+            return pages.map((page, index) => article(page.title, "Wikipedia", strip(page.snippet), index))
+        })
 }
