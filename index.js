9f7dac8eca16c282cdf1035a9ab9f89370fa36be
Raffi Sanna <rvs314@protonmail.com>
2019-05-25 19:46:24 -0400

    finished news

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 "use strict";
 
 function startquery() {
-    var query = document.getElementById("query").value
-    wikipedia(query).then((j) => {
-        console.log(j)
-    })
+    const query = document.getElementById("query").value
+    wikipedia(query).then((j) => console.log(j))
+    newsapi(query).then((j) => console.log(j))
 }
 
 function article(nm, src, blrb, rnk, url) {
-    let art = {}
+    const art = {}
     art.name = nm
     art.source = src
     art.blurb = blrb
@@ -30,11 +29,20 @@ function wikipedia(query) {
         .then(response => response.json())
         .then((j) => {
             const pages = j.query.search
-            return pages.map((page, index) => article(page.title, 
-                                                      "Wikipedia", 
-                                                      strip(page.snippet), 
-                                                      index,
-                                                      lookupurl + escape(page.title),
-                                                      ))
+            return pages.map((page, index) => article(page.title, "Wikipedia", strip(page.snippet), index, lookupurl + escape(page.title)))
         })
+}
+
+function newsapi(query) {
+    const newsapikey = "d943dcac77304701987917fb319681d9" // To be dealt with
+    const sources = "bbc-news,bbc-sport,associated-press"
+    const baseurl = "https://newsapi.org/v2/everything?language=en&sortBy=relevancy&pageSize=10"
+    const apiurl = baseurl + "&apikey="+newsapikey+"&sources="+sources+"&q="
+    const fullurl = apiurl + escape(query)
+    return fetch(fullurl)
+        .then(response => response.json())
+        .then((j) => {
+            const pages = j.articles
+            return pages.map((page, index) => article(page.title, page.source.name, page.description, index, page.url))
+       })
 }
